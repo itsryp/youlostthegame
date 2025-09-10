cdd92654324e38b8a13deb9de12127ac0cc0bb78
itsryp <droydumont@gmail.com>
2025-09-09 22:16:56 -0400

    reset button

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function addtolog() {
     }
     const ls = localStorage;
     let logdata = JSON.parse(ls.getItem('logdata')) || [];
-    const reasonstring = prompt("why did you lose??")
+    const reasonstring = prompt("how did you lose??")
     if (!reasonstring)
         return;
     let newentry = {
@@ -43,4 +43,22 @@ function addentry(entry) {
     const dateString = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear().toString().substring(2)}`;
     const row0 = document.querySelector('.table-headers');
     row0.insertAdjacentHTML('afterend', `<tr><td>${row0.parentElement.childElementCount}</td><td>${dateString}</td><td>${duration.toString().concat(duration==1?' Day':' Days')}</td><td class="reason"><i>"${entry.reason}"</i></td>`);
+}
+
+function clearlogs() {
+    if (localStorage.getItem('logdata') == null) {
+        alert('you dont have any logs');
+        return;
+    } else {
+        let confirmresult = confirm('reset data?');
+        if (confirmresult) {
+            let confirmresult2 = confirm('you sure?');
+            if (confirmresult2) {
+                localStorage.clear();
+                window.location.reload();
+            } else
+                return;
+        } else
+            return;
+    }
 }
